57cb08ecbfd29feee6a33dc06ab25bf39b646d39
Sina Dabiri <sina.dabiri@berkeley.edu>
2022-03-28 22:01:41 -0400

    pre-lab Q1 and 2 answers.

--- main.m
+++ main.m
@@ -15,18 +15,36 @@
 alpha = 90;
 T1 = [260, 780, 920, 3000];
 time = 0:0.01:20;
+figure("Name","Mz vs time")
 
 for p = 1:length(T1)
     M_z = zeros(1, length(time));
-    M_z(1) = -sqrt(2)/2;
+    Mz_mag = zeros(1, length(time));
+    M_z(1) = -1;
     for t = 2:length(time)
-       M_z(t) =  M_z(1)- M_z(1)*(- sqrt(2)/2*exp(-t/T1(p)));
-       
+       M_z(t) =  M_z(1)*exp(-t/T1(p))- M_z(1)*(1 - exp(-t/T1(p)));
+       Mz_mag(t) = abs(M_z(1)*exp(-t/T1(p))- M_z(1)*(1 - exp(-t/T1(p))));
+
     end
     hold on
+%     subplot(1,2,1)
+    figure(1)
     plot(time, M_z);
     xlabel("time (s)");
     ylabel("Mz");
+    hold off
+
+    hold on
+%     subplot(1,2,2)
+    figure(2)
+    plot(time, Mz_mag);
+    xlabel("time (s)");
+    ylabel("|Mz|");
+    hold off
+    
 end
-legend(["Fat", "WM", "GM", "CSF"])
 hold off
+figure(1)
+legend(["Fat", "WM", "GM", "CSF"])
+figure(2)
+legend(["Fat", "WM", "GM", "CSF"])
